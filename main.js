5c54b44a09e15874b96e77bca852a7eeb3523ec7
Your Name <you@example.com>
2021-05-07 15:57:56 +0200

    -

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Változó a WebSocket használatához
 var PORT = 3004
 //var PORT = process.env.PORT || 3003
-const  webSocket = new WebSocket("ws://https://webrtc-szakdolgozat.herokuapp.com")
+const  webSocket = new WebSocket("ws://webrtc-szakdolgozat.herokuapp.com")
 //const  webSocket = new WebSocket("ws://127.0.0.1:"+PORT)
 let ischrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)
 let localStream
