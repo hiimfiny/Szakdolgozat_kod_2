1226e757a729d241b36dd093d98246e5863ed4e3
Your Name <you@example.com>
2021-05-07 14:18:47 +0200

    -

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express")
 
 const app=express()
 var server = http.Server(app)
-app.use(express.static('public'))
+app.use(express.static('client'))
 
 //const server = http.createServer((req, res) => {})
 //var PORT = 3004
